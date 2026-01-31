9ab1a560d83a0da895f6f05eabf6efb2e2f1a87a
Hend Sayed <hendsayed853@gmail.com>
2026-01-31 19:17:46 +0200

    Add no internet connection screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.glide)
+    implementation(libs.lottie)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)

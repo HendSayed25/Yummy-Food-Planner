4d56fa1779eba9292c148332f3be8fe5dcfe7a26
Hend Sayed <hendsayed853@gmail.com>
2026-02-02 16:04:34 +0200

    Refactor: Restructure project packages for presentation layer and data based on MVP

---
+++ app/src/main/java/com/example/yummy_food_planner/presentation/favorite/view/FavMealAdapter.java
@@ -0,0 +1,57 @@
+package com.example.yummy_food_planner.presentation.favorite.view;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.yummy_food_planner.R;
+import com.example.yummy_food_planner.presentation.shared.model.Meal;
+import com.example.yummy_food_planner.presentation.shared.utils.GlideImageLoader;
+
+import java.util.List;
+
+public class FavMealAdapter extends RecyclerView.Adapter<FavMealAdapter.FavMealViewHolder> {
+
+    private List<Meal> favMeals;
+
+    public FavMealAdapter(List<Meal> favMeals) {
+        this.favMeals = favMeals;
+    }
+
+    public static class FavMealViewHolder extends RecyclerView.ViewHolder {
+        ImageView mealImage;
+        TextView mealName;
+
+        public FavMealViewHolder(View view) {
+            super(view);
+            mealImage = view.findViewById(R.id.ivMealImageFav);
+            mealName = view.findViewById(R.id.tvMealNameFav);
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public FavMealViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.favorite_item, parent, false);
+        return new FavMealViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FavMealViewHolder holder, int position) {
+        Meal favMeal = favMeals.get(position);
+
+        holder.mealName.setText(favMeal.getName());
+        GlideImageLoader.load(holder.itemView.getContext(), favMeal.getImageUrl(), holder.mealImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return favMeals.size();
+    }
+}
---
+++ app/src/main/java/com/example/yummy_food_planner/presentation/welcome/onboarding/OnboardingFragment2.java
@@ -0,0 +1,86 @@
+package com.example.yummy_food_planner.presentation.welcome.onboarding;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.navigation.Navigation;
+
+import com.example.yummy_food_planner.R;
+
+
+public class OnboardingFragment2 extends BaseOnboardingFragment {
+    private AppCompatButton nextBtn, skipBtn, backBtn;
+    private LinearLayout dotContainer;
+    private ImageView onboardingImage;
+    private TextView description;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_onboarding2, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        nextBtn = view.findViewById(R.id.nextBtn2);
+        skipBtn = view.findViewById(R.id.skipBtn2);
+        backBtn = view.findViewById(R.id.backBtn2);
+        dotContainer = view.findViewById(R.id.dotsContainer2);
+        onboardingImage = view.findViewById(R.id.onboardingImage2);
+        description = view.findViewById(R.id.descriptionTv2);
+
+        setupDots(dotContainer);
+        applyAnimations();
+
+        nextBtn.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_onboardingFragment2_to_onboardingFragment3);
+        });
+
+        skipBtn.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_onboardingFragment2_to_onboardingFragment3);
+        });
+
+        backBtn.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigateUp();
+        });
+    }
+
+    @Override
+    protected int getCurrentPage() {
+        return 1;
+    }
+
+    @Override
+    protected int getTotalPages() {
+        return 3;
+    }
+
+    private void applyAnimations() {
+        Animation slideIn = AnimationUtils.loadAnimation(requireContext(), R.anim.onboarding_slide_in_right);
+        onboardingImage.startAnimation(slideIn);
+
+        nextBtn.startAnimation(slideIn);
+
+        Animation slidInTop = AnimationUtils.loadAnimation(requireContext(), R.anim.onboarding_slide_in_top);
+        skipBtn.startAnimation(slidInTop);
+
+        Animation slidInLeft = AnimationUtils.loadAnimation(requireContext(), R.anim.onboarding_slid_in_left);
+        backBtn.startAnimation(slidInLeft);
+
+        Animation fadeIn = AnimationUtils.loadAnimation(requireContext(), R.anim.onboarding_fade_in);
+        fadeIn.setStartOffset(300);
+        description.startAnimation(fadeIn);
+    }
+}

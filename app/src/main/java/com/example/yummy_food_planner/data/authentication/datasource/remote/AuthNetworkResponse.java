228e4ab5223b4071b8246e3eedfa3e710492c756
HendSayed25 <hendsayed853@gmail.com>
2026-02-03 19:13:38 +0200

    rename success function

--- app/src/main/java/com/example/yummy_food_planner/data/authentication/datasource/remote/AuthNetworkResponse.java
+++ app/src/main/java/com/example/yummy_food_planner/data/authentication/datasource/remote/AuthNetworkResponse.java
@@ -1,6 +1,6 @@
 package com.example.yummy_food_planner.data.authentication.datasource.remote;
 
 public interface AuthNetworkResponse {
-    void onSignupSuccess(String userId);
+    void onSuccess(String userId);
     void onError(Exception exception);
 }

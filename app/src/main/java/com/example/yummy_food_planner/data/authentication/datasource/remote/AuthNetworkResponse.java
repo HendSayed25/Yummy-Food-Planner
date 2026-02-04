a3bda255d87fd9b11e9be854e4f6905eb22aad4c
HendSayed25 <hendsayed853@gmail.com>
2026-02-04 18:57:32 +0200

    Update AuthNetworkResponse to return User object

--- app/src/main/java/com/example/yummy_food_planner/data/authentication/datasource/remote/AuthNetworkResponse.java
+++ app/src/main/java/com/example/yummy_food_planner/data/authentication/datasource/remote/AuthNetworkResponse.java
@@ -1,6 +1,8 @@
 package com.example.yummy_food_planner.data.authentication.datasource.remote;
 
+import com.example.yummy_food_planner.data.authentication.model.User;
+
 public interface AuthNetworkResponse {
-    void onSuccess(String userId);
+    void onSuccess(User user);
     void onError(Exception exception);
 }

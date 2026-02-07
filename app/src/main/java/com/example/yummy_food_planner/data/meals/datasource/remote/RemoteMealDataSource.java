c90162e08ee776c12517a68c704643affc8468de
HendSayed25 <hendsayed853@gmail.com>
2026-02-07 14:23:11 -0800

    Expand MealRepository and its implementation

--- app/src/main/java/com/example/yummy_food_planner/data/meals/datasource/remote/RemoteMealDataSource.java
+++ app/src/main/java/com/example/yummy_food_planner/data/meals/datasource/remote/RemoteMealDataSource.java
@@ -1,5 +1,10 @@
 package com.example.yummy_food_planner.data.meals.datasource.remote;
 
+import com.example.yummy_food_planner.data.meals.datasource.remote.response.AreaListResponse;
+import com.example.yummy_food_planner.data.meals.datasource.remote.response.CategoriesResponse;
+import com.example.yummy_food_planner.data.meals.datasource.remote.response.CategoryListResponse;
+import com.example.yummy_food_planner.data.meals.datasource.remote.response.FilterMealResponse;
+import com.example.yummy_food_planner.data.meals.datasource.remote.response.IngredientsListResponse;
 import com.example.yummy_food_planner.data.meals.datasource.remote.response.MealResponse;
 import com.example.yummy_food_planner.data.network.NetworkClient;
 
@@ -20,5 +25,26 @@ public class RemoteMealDataSource {
 
     public Observable<MealResponse> getMealsByLetter(String letter){
         return mealService.getMealByFirstLetter(letter);
+    }
+    public Single<FilterMealResponse> getMealsByCountry(String c){
+        return mealService.getMealByArea(c);
+    }
+    public Single<FilterMealResponse> getMealsByCategory(String c){
+        return mealService.getMealByCategory(c);
+    }
+    public Single<FilterMealResponse> getMealsByIngredient(String i){
+       return mealService.getMealByMainIngredient(i);
+    }
+    public Observable<MealResponse> getMealByName(String name){
+        return mealService.getMealByName(name);
+    }
+    public Single<CategoriesResponse> getAllCategories(){
+        return mealService.getMealCategories();
+    }
+    public Single<AreaListResponse> getAllCountries(String c){
+        return mealService.getMealAreaList(c);
+    }
+    public Single<IngredientsListResponse> getAllIngredients(String i){
+        return mealService.getMealIngredientsList(i);
     }
 }
--- app/src/main/java/com/example/yummy_food_planner/data/meals/datasource/remote/repository/MealRepository.java
+++ app/src/main/java/com/example/yummy_food_planner/data/meals/datasource/remote/repository/MealRepository.java
@@ -1,5 +1,10 @@
 package com.example.yummy_food_planner.data.meals.datasource.remote.repository;
 
+import com.example.yummy_food_planner.data.meals.datasource.remote.response.AreaListResponse;
+import com.example.yummy_food_planner.data.meals.datasource.remote.response.CategoriesResponse;
+import com.example.yummy_food_planner.data.meals.datasource.remote.response.CategoryListResponse;
+import com.example.yummy_food_planner.data.meals.datasource.remote.response.FilterMealResponse;
+import com.example.yummy_food_planner.data.meals.datasource.remote.response.IngredientsListResponse;
 import com.example.yummy_food_planner.data.meals.datasource.remote.response.MealResponse;
 
 import io.reactivex.rxjava3.core.Observable;
@@ -8,4 +13,11 @@ import io.reactivex.rxjava3.core.Single;
 public interface MealRepository {
     Single<MealResponse> getRandomMeal();
     Observable<MealResponse> getMealsByLetter(String letter);
+    Single<FilterMealResponse> getMealsByCountry(String c);
+    Single<FilterMealResponse> getMealsByCategory(String c);
+    Single<FilterMealResponse> getMealsByIngredient(String i);
+    Observable<MealResponse> getMealByName(String name);
+    Single<CategoriesResponse> getAllCategories();
+    Single<AreaListResponse> getAllCountries(String c);
+    Single<IngredientsListResponse> getAllIngredients(String i);
 }

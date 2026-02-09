2203ac421efa1e357d372ffa1714a25d5702604a
HendSayed25 <hendsayed853@gmail.com>
2026-02-09 11:53:26 -0800

    Implement Room database for local data storage

---
+++ app/src/main/java/com/example/yummy_food_planner/data/meals/datasource/local/MealDao.java
@@ -0,0 +1,25 @@
+package com.example.yummy_food_planner.data.meals.datasource.local;
+
+import static com.example.yummy_food_planner.data.meals.datasource.local.RoomConstants.MEAL_TABLE_NAME;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.yummy_food_planner.data.model.entitiy.Meal;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Observable;
+
+public interface MealDao {
+
+    @Query("SELECT * FROM " + MEAL_TABLE_NAME)
+    Observable<List<Meal>> getAllFavorite();
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Completable addToFavorite(Meal meal);
+    @Delete
+    Completable deleteFromFavorite(Meal meal);
+}
---
+++ app/src/main/java/com/example/yummy_food_planner/data/meals/datasource/local/RoomConstants.java
@@ -0,0 +1,7 @@
+package com.example.yummy_food_planner.data.meals.datasource.local;
+
+public class RoomConstants {
+    public final static String DATABASE_NAME = "AppDb";
+    public final static String MEAL_TABLE_NAME = "Meals";
+    public final static String PLAN_TABLE_NAME = "Plans";
+}

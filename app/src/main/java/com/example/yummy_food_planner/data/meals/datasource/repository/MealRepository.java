8647cb5f7e52d302cbfb07d8733517266fe58c74
HendSayed25 <hendsayed853@gmail.com>
2026-02-09 12:40:27 -0800

    Refactor: Integrate local and remote data sources in MealRepository

--- app/src/main/java/com/example/yummy_food_planner/data/meals/datasource/repository/MealRepository.java
+++ app/src/main/java/com/example/yummy_food_planner/data/meals/datasource/repository/MealRepository.java
@@ -1,4 +1,4 @@
-package com.example.yummy_food_planner.data.meals.datasource.remote.repository;
+package com.example.yummy_food_planner.data.meals.datasource.repository;
 
 import com.example.yummy_food_planner.data.meals.datasource.remote.response.AreaListResponse;
 import com.example.yummy_food_planner.data.meals.datasource.remote.response.CategoriesResponse;
@@ -6,20 +6,47 @@ import com.example.yummy_food_planner.data.meals.datasource.remote.response.Cate
 import com.example.yummy_food_planner.data.meals.datasource.remote.response.FilterMealResponse;
 import com.example.yummy_food_planner.data.meals.datasource.remote.response.IngredientsListResponse;
 import com.example.yummy_food_planner.data.meals.datasource.remote.response.MealResponse;
+import com.example.yummy_food_planner.data.model.entitiy.Meal;
+import com.example.yummy_food_planner.data.model.entitiy.Plan;
 
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Single;
 
 public interface MealRepository {
     Single<MealResponse> getRandomMeal();
+
     Observable<MealResponse> getMealsByLetter(String letter);
+
     Single<FilterMealResponse> getMealsByCountry(String c);
+
     Single<FilterMealResponse> getMealsByCategory(String c);
+
     Single<FilterMealResponse> getMealsByIngredient(String i);
+
     Observable<MealResponse> getMealByName(String name);
+
     Single<CategoriesResponse> getAllCategories();
+
     Single<AreaListResponse> getAllCountries(String c);
+
     Single<IngredientsListResponse> getAllIngredients(String i);
-    public Single<MealResponse> getMealDetailsById(String mealId);
-    public String getIngredientImageUrl(String ingredientName);
+
+    Single<MealResponse> getMealDetailsById(String mealId);
+
+    String getIngredientImageUrl(String ingredientName);
+
+    Observable<List<Meal>> getAllFavoriteMeals();
+
+    Observable<List<Plan>> getMealPlansByDate(Long date, String userId);
+
+    Completable addMealToFavorite(Meal meal, String userId);
+
+    Completable deleteMealFromFavorite(String mealId, String userId);
+
+    Completable addMealToPlan(Plan meal, Long date, String userId);
+
+    Completable deleteMealFromPlan(String mealId, Long date, String userId);
 }

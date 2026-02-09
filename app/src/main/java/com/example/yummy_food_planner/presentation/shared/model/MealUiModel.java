b912798531d264f44956b29ce519dd7aa69b6863
HendSayed25 <hendsayed853@gmail.com>
2026-02-08 21:27:59 -0800

    feat: Add id to MealUiModel

--- app/src/main/java/com/example/yummy_food_planner/presentation/shared/model/MealUiModel.java
+++ app/src/main/java/com/example/yummy_food_planner/presentation/shared/model/MealUiModel.java
@@ -3,10 +3,12 @@ package com.example.yummy_food_planner.presentation.shared.model;
 public class MealUiModel {
     private String name;
     private String imageUrl;
+    private String id;
 
-    public MealUiModel(String name, String imageUrl) {
+    public MealUiModel(String name, String imageUrl, String id) {
         this.name = name;
         this.imageUrl = imageUrl;
+        this.id = id;
     }
 
     public String getName() {
@@ -15,5 +17,9 @@ public class MealUiModel {
 
     public String getImageUrl() {
         return imageUrl;
+    }
+
+    public String getId() {
+        return id;
     }
 }

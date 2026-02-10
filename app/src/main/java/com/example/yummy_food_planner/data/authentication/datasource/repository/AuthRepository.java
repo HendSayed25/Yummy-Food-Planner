c20faaaf237dc85b3828f9db347f1ed4af377d27
HendSayed25 <hendsayed853@gmail.com>
2026-02-10 11:17:26 -0800

    Implement AuthLocalDataSource using SharedPreferences

--- app/src/main/java/com/example/yummy_food_planner/data/authentication/datasource/repository/AuthRepository.java
+++ app/src/main/java/com/example/yummy_food_planner/data/authentication/datasource/repository/AuthRepository.java
@@ -8,7 +8,24 @@ import io.reactivex.rxjava3.core.Single;
 
 public interface AuthRepository {
     Single<User> signup(String email, String password);
+
     Completable signInWithEmailAndPassword(String email, String password);
+
     Single<User> signInWithGoogle(GoogleSignInAccount account);
+
     Completable saveUserData(User user);
+
+    Single<Boolean> isUserLoggedIn();
+
+    Single<Boolean> isFirstTime();
+
+    Single<String> getUserName();
+
+    Single<String> getUserId();
+
+    Completable logout();
+
+    Completable setLoggedIn();
+
+    Completable setIsNotFirstTime();
 }

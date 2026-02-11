509b5a5a01fc0f39867da96093c691b47eac9296
HendSayed25 <hendsayed853@gmail.com>
2026-02-10 19:19:09 -0800

    Add CalenderUtils helper

---
+++ app/src/main/java/com/example/yummy_food_planner/presentation/shared/utils/CalenderUtils.java
@@ -0,0 +1,20 @@
+package com.example.yummy_food_planner.presentation.shared.utils;
+
+import java.util.Calendar;
+
+
+public class CalenderUtils {
+    public static long getSelectedDateInMillis(int year, int month, int day) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month);
+        cal.set(Calendar.DAY_OF_MONTH, day);
+
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        return cal.getTimeInMillis();
+    }
+}
